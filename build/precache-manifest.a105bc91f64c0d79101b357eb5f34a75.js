b4a6bde47270b872308c1f633c583d889d77cc42
Nifnifas <lkrupenkinas@gmail.com>
2019-09-09 22:58:46 +0300

    naujas

---
+++ build/precache-manifest.a105bc91f64c0d79101b357eb5f34a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfa6b75be7ceaef6438094e177eb953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9f6ff93576aa242909",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "012deaaa9f23fddf58ea",
+    "url": "/static/js/2.1e638ba1.chunk.js"
+  },
+  {
+    "revision": "7c9f6ff93576aa242909",
+    "url": "/static/js/main.0f455fc5.chunk.js"
+  },
+  {
+    "revision": "055cf71f9e51257fb1f3",
+    "url": "/static/js/runtime~main.0cc3c789.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
